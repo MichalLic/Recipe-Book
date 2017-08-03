b88d35a72244dad0ad369c273c2bb9ba3dcd6bf9
Michal <m14lickiewicz@gmail.com>
2017-08-03 10:38:58 +0200

    Use firebase to logout/login state.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   loadedFeature = 'recipe';
+
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyBe7_Dcmmm60h6nXQUSeAHlerAfczMYduA',
+      authDomain: 'ng-recipe-book-8d5a1.firebaseapp.com',
+    });
+  }
 
   onNavigate(featured: string) {
     this.loadedFeature = featured;
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,7 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Response} from '@angular/http';
+import {DataStorageService} from '../data-storage.service';
+import {AuthService} from '../auth/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -7,10 +10,28 @@ import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() {
+  constructor(private dataStorageService: DataStorageService,
+              private authService: AuthService) {
   }
 
   ngOnInit() {
+  }
+
+  onSave() {
+    this.dataStorageService.storeRecipe()
+      .subscribe(
+        (response: Response) => {
+          console.log(response);
+        }
+      );
+  }
+
+  onFetchData() {
+    this.dataStorageService.getRecipes();
+  }
+
+  onLogout() {
+    this.authService.logout();
   }
 
 }
